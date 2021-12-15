40bfdeeb97c749f61f51db5306e1c7e7ea81b4be
Shivansh Sharma <52012879+Shivansh-005@users.noreply.github.com>
2021-12-15 23:39:50 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
-const path = require("path");
-
+ const path = require("path");
+require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -11,11 +12,12 @@ module.exports = {
       network_id: "5777"
     },
     ropsten: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: 3,
+     // host: "127.0.0.1",
+     // port: 8545,
+provider: () => new HDWalletProvider(process.env.TEST_MNEMONIC, "https://ropsten.infura.io/v3/279c40b247e246609188d276e02e6de2", 0),
+network_id: 3,
       gas: 4700000,
-      from: "0x7fC348b217b1F02fBa88e7b5096C9eA5D9C6E8A0"
+    //  from: "0x7fC348b217b1F02fBa88e7b5096C9eA5D9C6E8A0"
     },
     rinkeby: {
       host: "127.0.0.1",
@@ -26,3 +28,24 @@ module.exports = {
     }
   }
 };
+
+/*require('dotenv').config();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+module.exports = {
+contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+        networks: {
+                development: {
+                        host: "127.0.0.1",
+                        port: 8545,
+                        network_id: "*"
+                },
+                "ropsten-infura": {
+                        provider: () => new HDWalletProvider(process.env.TEST_MNEMONIC, "https://ropsten.infura.io/"+process.env.INFURA_KEY, 0),
+                        network_id: 3,
+                        gas: 4700000,
+                        gasPrice: 100000000000
+                }
+        }
+};*/
+
